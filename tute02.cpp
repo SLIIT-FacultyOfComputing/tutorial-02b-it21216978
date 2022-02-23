842a09b4b0b3a9c39da17d9f49794b9a776ed0d7
it21216978 <99277600+it21216978@users.noreply.github.com>
2022-02-23 11:53:58 +0000

    Tutorial 2

--- tute02.cpp
+++ tute02.cpp
@@ -1,30 +1,20 @@
-/*Exercise 2 - Selection
-
-Convert the C program given below which calculates an employee's salary to a C++ program.
-
-Input Type, Salary, otHours
-
-Type = 1
-OtRate = 1000
-Type = 2
-OtRate = 1500
-Type = 3
-OtRate = 1700
-Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
-
-#include <stdio.h>
+#include<iostream>
 int main()
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
+  float salary,netSalary;
+  int etype ,otHrs,otRate;
+
+  std::cout<<"Enter Employee Type :";
+  std::cin>>etype;
+
+  std::cout<<"Enter Salary :";
+  std::cin>>salary;
+
+  std::cout<<"Enter OtHrs :";
+  std::cin>>otHrs;
+
+  switch (etype) 
+  {
       case 1 :
           otRate = 1000;
           break;
@@ -36,9 +26,11 @@ int main()
           break;
    }
 
-
    netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
-  
-   return 0;
+
+  std::cout<<"Net Salary is :"<<netSalary;
+
+
+  return 0;
 }
+
--- tute04.cpp
+++ tute04.cpp
@@ -1,15 +1,3 @@
-/*Exercise 4 - Functions
-
-Write a program to calculate the function called nCr which is defined as
-
-nCr = n!/ r!(n−r)!
-Where n! is the factorial of n.
-
-Implement the functions
-
-long Factorial(int no);
-long nCr(int n, int r);
-Do not modify the main function.*/
 
 #include <iostream>
 
@@ -22,8 +10,24 @@ int main() {
   std::cin >> n;
   std::cout << "Enter a value for r ";
   std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
+  std::cout << "nCr = "<< nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+long Factorial(int no)
+{
+  int fac = 1;
+  for (int x=no; x >= 1; x--) 
+    {
+        fac = fac * x;
+    }
+  return fac;
+}
+
+
+long nCr(int n, int r)
+{
+  long nCr = Factorial(n)/(Factorial(r) * Factorial(n-r));
+ 
+  return nCr;
 }
